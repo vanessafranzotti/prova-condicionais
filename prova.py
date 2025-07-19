12ddf3cbcc7cc586eb1208cd1a1072ee9852d376
Vanessa Franzotti <vanessafranzotti@gmail.com>
2025-07-18 22:14:18 -0300

    adicionado o código

---
+++ prova.py
@@ -0,0 +1,9 @@
+numero = int (input("Digite um número: "))
+
+if numero > 0:
+    print("O número é positivo")
+elif numero <0 :
+    print ("O número é negativo")
+else:
+    mensagem = 0
+    print(f" O número digitado é {mensagem}")
